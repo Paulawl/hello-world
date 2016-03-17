f42f3dcc3a5c61d6de53ca09a07750fd40af9416
Paul Magliozzi <pmagliozzi@gmail.com>
2016-03-17 17:52:01 -0400

    Adding an actual hello world file

---
+++ HelloWorldC++.cpp
@@ -0,0 +1,15 @@
+/***********************************************/
+/* Originally written by: Paul Magliozzi       */
+/* 3/17/2016								   */
+/*											   */
+/* Hello World C++							   */
+/***********************************************/
+
+#include "stdio.h"
+
+int main()
+{
+	printf("Hello World!");
+
+	return 0;
+}
